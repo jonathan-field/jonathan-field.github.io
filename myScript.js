e7cf5d586d234596c853fc3e92db50b51eaad385
jonathan-field <jonathanfield90@yahoo.com>
2021-08-12 16:22:47 -0400

    Add style to Back to Top

--- myScript.js
+++ myScript.js
@@ -38,42 +38,43 @@ scrollToTopButton.onclick = function(e)
   scrollToTop();
 }
 
+// The following will allow the Back to Top button to override Safari's bottom bar.
 /**
  * 44px is the height of the Safari overflow bar
  */
- const SAFARI_MOBILE_BOTTOM_MENU_HEIGHT = 44;
+const SAFARI_MOBILE_BOTTOM_MENU_HEIGHT = 44;
 
- /**
-  * Import from a library such as lodash or implement your own. Scroll events
-  * should always be throttled or debounced.
-  */
- const throttle = () => {};
+/**
+ * Import from a library such as lodash or implement your own. Scroll events
+ * should always be throttled or debounced.
+ */
+const throttle = () => {};
  
- /**
-  * When bottom > window.innerHeight, we know for a fact that `element` is
-  * outside the window bounds. Normally, `position: fixed` keeps the element
-  * inside the window bounds.
-  *
-  * @param {Element} element
-  * @param {Number} offsetBottom - in pixels
-  */
- const reactToSafariOverflowBar = (element, offsetBottom) => {
-   const {bottom} = element.getBoundingClientRect();
-   if (Math.floor(bottom) > window.innerHeight) {
-     element.style.bottom = offsetBottom + SAFARI_MOBILE_BOTTOM_MENU_HEIGHT;
-   } else {
-     element.style.bottom = offsetBottom;
-   }
- };
+/**
+ * When bottom > window.innerHeight, we know for a fact that `element` is
+ * outside the window bounds. Normally, `position: fixed` keeps the element
+ * inside the window bounds.
+ *
+ * @param {Element} element
+ * @param {Number} offsetBottom - in pixels
+ */
+const reactToSafariOverflowBar = (element, offsetBottom) => {
+    const {bottom} = element.getBoundingClientRect();
+        if (Math.floor(bottom) > window.innerHeight) {
+            element.style.bottom = offsetBottom + SAFARI_MOBILE_BOTTOM_MENU_HEIGHT;
+        } else {
+            element.style.bottom = offsetBottom;
+        }
+};
+
+const offsetBottom = 20;
+const stuckToBottom = document.querySelector('.is-stuckToBottom');
  
- const offsetBottom = 20;
- const stuckToBottom = document.querySelector('.is-stuckToBottom');
- 
- /**
-  * Handle scroll events. This listener should be removed if the component
-  * is removed, but this serves its demo purposes.
-  */
- window.addEventListener('scroll', throttle(() => reactToSafariOverflowBar(stuckToBottom, offsetBottom), 200));
+/**
+ * Handle scroll events. This listener should be removed if the component
+ * is removed, but this serves its demo purposes.
+ */
+window.addEventListener('scroll', throttle(() => reactToSafariOverflowBar(stuckToBottom, offsetBottom), 200));
 
 function updateRequirement(weapons) 
 {
